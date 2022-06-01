c89fceda33e8b5782d284be9773a012c08b859c0
Avinash katariya <44751905+avinashkatariya@users.noreply.github.com>
2022-06-01 03:16:30 +0000

    removed output txt

--- main.py
+++ main.py
@@ -8,16 +8,12 @@ def main():
   #creating parkingLot Instance
   parkingLot = ParkingLot()
   
-  outputFile = open('output.txt','w')
-  
   for command in readLines:
     command = command.replace("\n","")
     args = command.split(" ")
     func = parkingLot.getFunctionFromString(args[0])
     output = func(*args[1:])
-    outputFile.write(str(output)+"\n")
-  
-  outputFile.close()
+    print(output)
 
 if __name__ == '__main__':
   main()
